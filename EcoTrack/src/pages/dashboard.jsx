f1cd086a91fa2d6b5a868ed0e97f85bec90082cd
alishaa1731@gmail.com <alishaa1731@gmail.com>
2026-01-20 22:52:00 +0530

    Experiment 1: EcoTrack Foundation

---
+++ EcoTrack/src/pages/dashboard.jsx
@@ -0,0 +1,24 @@
+import logs from "../data/logs";
+
+const Dashboard = () => {
+    const totalCarbon = logs.reduce((sum, log)=> sum+log.carbon, 0);
+
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <p>Total Carbon Foootprint: {totalCarbon} Kg</p>
+
+            <ul>
+                {
+                    logs.map((log)=>(
+                        <li key={log.id}>
+                            {log.activity} - {log.carbon}Kg
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Dashboard;
